b4f1b0fe921246a3d62b3a60f30fa39c0d8bcdf8
Cedo Developer <62746646+Cedtron@users.noreply.github.com>
2026-01-20 00:33:06 +0300

    url change

--- src/main.ts
+++ src/main.ts
@@ -64,10 +64,11 @@ process.on('unhandledRejection', (reason) => {
   console.error('UNHANDLED REJECTION', reason);
 });
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
-  console.log(`Swagger documentation available at: http://localhost:${port}/api`);
+const port = process.env.PORT || 3000;
+await app.listen(port, '0.0.0.0');
+
+console.log(`Application is running on: http://0.0.0.0:${port}`);
+console.log(`Swagger documentation available at: http://0.0.0.0:${port}/api`);
 }
 bootstrap();
 
